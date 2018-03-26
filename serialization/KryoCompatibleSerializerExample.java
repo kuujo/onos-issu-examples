680adea421d042ff646ae85bc9c4051220a40bde
Jordan Halterman <jordan.halterman@gmail.com>
2018-03-25 19:27:22 -0700

    Clean up KryoCompatibleSerializerExample

--- serialization/KryoCompatibleSerializerExample.java
+++ serialization/KryoCompatibleSerializerExample.java
@@ -9,25 +9,6 @@ public class KryoCompatibleSerializerExample {
         .register(NodeId.class)
         .build());
 
-    private static class HeartbeatMessageSerializer extends com.esotericsoftware.kryo.Serializer<HeartbeatMessage> {
-        private final byte VERSION = 1;
-
-        @Override
-        public void write(Kryo kryo, Output output, HeartbeatMessage message) {
-            output.writeByte(VERSION);
-            kryo.writeObject(output, message.source());
-            kryo.writeObject(output, message.state());
-        }
-
-        @Override
-        public HeartbeatMessage read(Kryo kryo, Input input, Class<HeartbeatMessage> type) {
-            byte version = input.readByte();
-            ControllerNode source = kryo.readObject(input, ControllerNode.class);
-            ControllerNode.State state = kryo.readObject(input, ControllerNode.State.class);
-            return new HeartbeatMessage(source, state);
-        }
-    }
-
     private static class HeartbeatMessage {
         private ControllerNode source;
         private State state;
